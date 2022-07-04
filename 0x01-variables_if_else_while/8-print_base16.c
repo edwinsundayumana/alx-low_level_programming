83591556927f4ff8aae107cc5a4706e5fe2790bf
EDWIN SUNDAY UMANA <umanaedwin247@gmail.com>
2022-07-04 05:04:19 -0700

    8th task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbersof base 16 in lower case
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int num;
+char letter;
+
+for (num = 0; num < 10; num++)
+	putchar((num % 10) + '0');
+for (letter = 'a'; letter <= 'f'; letter++)
+	putchar(letter);
+putchar('\n');
+return (0);
+}
