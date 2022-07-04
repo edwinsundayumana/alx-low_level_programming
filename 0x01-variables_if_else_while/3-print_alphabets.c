f1b736b4298773b7d3d6702b5f60a3b856c7a651
EDWIN SUNDAY UMANA <umanaedwin247@gmail.com>
2022-07-04 05:15:58 -0700

    3rd task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,10 @@ int main(void)
 {
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
-putchar(letter);
+	putchar(letter);
 for (letter = 'A'; letter <= 'Z'; letter++)
-putchar(letter);
-putchar('n');
+	putchar(letter);
+
+putchar('\n');
 return (0);
 }
