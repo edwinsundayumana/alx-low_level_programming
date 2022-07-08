77e681629e39298929efc8e90d14a0f41061ae22
EDWIN SUNDAY UMANA <umanaedwin247@gmail.com>
2022-07-08 07:00:12 -0700

    3rd task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from 0 to 9 followed by a new line
+ * print_numbers - print numbers from 0 to 9 followed by a new line
  * Return: no return
  */
 
 void print_numbers(void)
 {
 	int i;
-	for (i = 48; i<= 57; i++)
+
+	for (i = 48; i <= 57; i++)
 	{
 		_putchar(i);
 	}
 	_putchar('\n');
+
 }
