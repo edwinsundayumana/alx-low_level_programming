32579fb88d4d3535593bb58538639161acc7eaff
EDWIN SUNDAY UMANA <umanaedwin247@gmail.com>
2022-07-08 06:48:07 -0700

    first task

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This contains the solutions for the task on more functions and nested loops
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,60 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _putchar - writes the character c to stdout
+ */
+int _putchar(char c);
+
+/**
+ * _isupper - checks for uppercase character
+ */
+int _isupper(int c);
+
+/**
+ * _isdigit - checks for digit 0 through 9
+ */
+int _isdigit(int c);
+
+/**
+ * mul - multipies two integers
+ */
+int mul(int a,int b);
+
+/**
+ * print_most_numbers - print numbers from 0 to 9 except 2 and 4
+ */
+void print_most_numbers(void);
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ */
+void more_numbers(void);
+
+/**
+ * print_square - prints a square followed by a new line
+ */
+void print_square(int size);
+
+/**
+ * print_line - draws a straight line in the terminal
+ */
+void print_line(int n);
+
+/**
+ * print_diagonal - drws a diagonalline in the terminal
+ */
+void print_diagonal(int n);
+
+/**
+ * fizz_buzz - prints numbers from 1 to 100 followed by a new line
+ * for multiple of three print Fizz
+ * for multiple of five print Buzz
+ * for numbers that are multiples of both three and five print FizzBuzz
+ */
+
+/**
+ * print_triangle - prints a triangle followed by a new line
+ */
+void print_triangle(int size);
+void print_number(int n);
+#endif /*MAIN_H*/
