bc820493293b4706147509bc703454704d8a4899
EDWIN SUNDAY UMANA <umanaedwin247@gmail.com>
2022-07-13 03:43:48 -0700

    reverse task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,36 +3,30 @@
 /**
  * rev_string - prints a reverse string
  * @s: A pointer to an int that will be changed
+ * Description: Reveses the string
  * Return: void
  */
 
 void rev_string(char *s)
 {
-	char *start_c, *end_c, c;
-	int i, count;
-	int length = 0;
+	int i = 0;
 
-	for (i = 0; s[i]; i++)
+	int j = 0;
+
+	char temp[1000];
+
+	while (s[i] != '\0')
 	{
-		length++;
+		i++;
 	}
-
-	count = length;
-
-	start_c = s;
-	end_c = s;
-
-	for (i = 0; i < count - 1; i++)
+	for (; i > 0; i--)
 	{
-		endc++;
+		temp[j] = s[i - 1];
+		j++;
 	}
-	for (i = 0; i < count / 2; i++)
+	while (i < j)
 	{
-		c = *end_c;
-		*end_c = *start_c;
-		*start_c = c;
-
-		start_c++;
-		end_c--;
+		s[i] = temp[i];
+		i++;
 	}
 }
