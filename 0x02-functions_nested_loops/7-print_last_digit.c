283f3b0420c8d81b995ed307380f52f765ac7497
EDWIN SUNDAY UMANA <umanaedwin247@gmail.com>
2022-07-13 03:51:59 -0700

    seveth task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,16 +8,9 @@
 
 int print_last_digit(int n)
 {
-	int a;
-
+	n = (n % 10);
 	if (n < 0)
 		n = -n;
-
-	a = n % 10
-
-	if (a < 0)
-		a = -a;
-	_putchar(a + '0');
-
-	return (a);
+	_putchar(n + '0');
+	return (n);
 }
