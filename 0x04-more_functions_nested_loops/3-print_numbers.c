311352f170e2a0a8055ae91b004ff11d134d8355
EDWIN SUNDAY UMANA <umanaedwin247@gmail.com>
2022-07-13 09:58:25 -0700

    third task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,13 +4,12 @@
  * print_numbers - prints numbers from 0 to 9 followed by a new line
  * Return: no return
  */
+
 void print_numbers(void)
 {
-	int i;
+	char i;
 
-	for (i = 48; i <= 57; i++)
-	{
+	for (i = '0'; i <= '9'; i++)
 		_putchar(i);
-	}
 	_putchar('\n');
 }
