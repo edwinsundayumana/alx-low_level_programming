b08f1c037188f38ddceed99f6c2295e976a4df1c
EDWIN SUNDAY UMANA <umanaedwin247@gmail.com>
2022-08-10 05:43:04 -0700

    advc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,19 +1,15 @@
 #include "main.h"
 
 /**
-* main - multiplies two positive numbers
-* @argc: n arguments
-* @argv: args
-* Return: int
-*/
-
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
+ */
 int main(int argc, char *argv[])
 {
-
 unsigned long mul;
-
 int i, j;
-
 	if (argc != 3)
 	{ printf("Error\n");
 	exit(98); }
