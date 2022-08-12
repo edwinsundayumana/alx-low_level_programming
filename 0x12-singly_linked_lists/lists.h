022ada148896a0bbfcf1a4f49170a33d9104014f
EDWIN SUNDAY UMANA <umanaedwin247@gmail.com>
2022-08-12 07:03:08 -0700

    task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -16,10 +16,11 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+}
+list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
