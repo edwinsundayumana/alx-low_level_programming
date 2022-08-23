3b3837a3a71984643e4f6f427e8bc6091a8aff71
EDWIN SUNDAY UMANA <umanaedwin247@gmail.com>
2022-08-23 01:16:06 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,51 +1,88 @@
-#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int safe_close(int);
+/**
+* main - Main function to copy files
+* @argc: The number of passed arguments
+* @argv: The pointers to array arguments
+* Return: 1 on success, exits on failure
+*/
+int main(int argc, char *argv[])
+{
+char buffer[1024];
+int bytes_read = 0, _EOF = 1, from_fd = -1, to_fd = -1, error = 0;
+
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+from_fd = open(argv[1], O_RDONLY);
+if (from_fd < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+
+to_fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+if (to_fd < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+safe_close(from_fd);
+exit(99);
+}
+
+while (_EOF)
+{
+_EOF = read(from_fd, buffer, 1024);
+if (_EOF < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+safe_close(from_fd);
+safe_close(to_fd);
+exit(98);
+}
+else if (_EOF == 0)
+break;
+bytes_read += _EOF;
+error = write(to_fd, buffer, _EOF);
+if (error < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+safe_close(from_fd);
+safe_close(to_fd);
+exit(99);
+}
+}
+error = safe_close(to_fd);
+if (error < 0)
+{
+safe_close(from_fd);
+exit(100);
+}
+error = safe_close(from_fd);
+if (error < 0)
+exit(100);
+return (0);
+}
 
 /**
- * main - copies the content of a file to another file
- * @argc: number of arguments passed
- * @argv: double pointer
- * Return: the actual number of letters it could read and print
- */
-int main(int argc, char **argv)
+* safe_close - A function that closes a file and prints error when closed file
+* @description: Description error for closed file
+* Return: 1 on success, -1 on failure
+*/
+int safe_close(int description)
 {
-	int f1, f2, n;
-	char buf[1024];
+int error;
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	f1 = open(argv[1], O_RDONLY);
-	if (f1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	f2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	while ((n = read(f1, buf, 1024)) > 0)
-	{
-		if (write(f2, buf, n) != n || f2 == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-	}
-	if (n == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (close(f1) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
-		exit(100);
-	}
-	if (close(f2) < 0)
-	{
-		printf(STDERR_FILENO, "Error: Can't close fd %d\n", f2);
-		exit(100);
-	}
-	return (0);
+error = close(description);
+if (error < 0)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", description);
+return (error);
 }
